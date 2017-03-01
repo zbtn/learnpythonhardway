5dc77038772c390130002cf8c4a02e239bd03056
zbtn <zbytnievzky@gmail.com>
2017-03-01 21:09:17 +0100

    [ex1] Express my enjoyment of printing in Python

--- ex1.py
+++ ex1.py
@@ -6,4 +6,6 @@ print "I like typing this."
 print "This is fun."
 print 'Yay! Printing.'
 print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
+print 'Dzień dobry.'
+print "Oczywiście, żę też się cieszę, głupcze."
+# print 'I "said" do not touch this.'
